ced369e20086ecef99454661ae28bd1f8fee9474
syanhai_yakisoba=\chan! <44153315+koumaza@users.noreply.github.com>
2020-04-18 21:36:10 +0900

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+ls -al */*/
 
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
-env GO111MODULE=on go get -v github.com/mikefarah/yq/v3
 
-curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo
-chmod a+x ./repo && export PATH=~$(pwd):$PATH
+export repourl= $(cat onfig.yml|yq .repourl[])
+export branch=  $(cat config.yml|yq .branch[])
+
+env GO111MODULE=on go get -v github.com/mikefarah/yq/v3
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo ; chmod a+x ./repo
+
 mkdir build && cd build
-../repo init -u $(cat ../config.yml|yq .repourl[]) -b $(cat ../config.yml|yq .branch[])
+../repo init -u $repourl-b $branch
 ../repo sync --no-tags --no-clone-bundle --force-sync -j$(grep processor /proc/cpuinfo | wc -l)
 . build/envsetup.sh
 
