d093433e47d66bc9a3fd4501e75d8173a5ed3ab0
syanhai_yakisoba=\chan! <44153315+koumaza@users.noreply.github.com>
2020-04-18 16:13:05 +0900

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo && export PATH=~/bin/repo:$PATH
+mkdir build && cd build
+~/bin/repo init -u $(cat ../config.yml|yq .repourl) -b $(cat ../config.yml|yq .branch)
+~/bin/repo sync --no-tags --no-clone-bundle --force-sync -j$(grep processor /proc/cpuinfo | wc -l)
+
+$(cat ../config.yml|yq -c .build_command[])
+
+. build/envsetup.sh
