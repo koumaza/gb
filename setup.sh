8e05eccc98da87bdbcfcd3385840325d01a45418
syanhai_yakisoba=\chan! <44153315+koumaza@users.noreply.github.com>
2020-04-22 05:35:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update  -y
-sudo apt install -y nodejs npm git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386  lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip squashfs-tools python-mako libssl-dev ninja-build lunzip syslinux syslinux-utils gettext genisoimage gettext bc xorriso
+sudo apt install -y nodejs npm
 sudo npm install n -g
 sudo n latest
 sudo apt purge -y nodejs npm
